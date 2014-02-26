e1f1c86efba8a5a796b151e572040a175859aae1
Amy <amy.tsai@berkeley.edu>
2014-02-25 23:06:44 -0800

    Q3

--- hw3.md
+++ hw3.md
@@ -17,6 +17,7 @@ Errors =
 0.9092]
 
 d = 10 has the smallest error rate. However, in practice using d = 10 will probably over fit. d = 3 is most likely the best choice.
+![Q1a](Q1a.png)
 
 ### (B)
 Test error (d=3) = 5.1577
@@ -24,3 +25,40 @@ Test error (d=3) = 5.1577
 Test error (d=10) = 5.7307
 
 A degree 10 polynomial is overfitted to the training data and a degree 3 polynomial generalizes better to the test data.
+
+Question 2
+----------
+### (i)
+![Q2i](Q2i.png)
+
+### (ii)
+![Q2ii](Q2ii.png)
+
+### (iii)
+![Q2iii](Q2iii.png)
+
+### (iv)
+![Q2iv](Q2iv.png)
+
+### (v)
+![Q2v](Q2v.png)
+
+Question 3
+----------
+### (i)
+The MLE estimates of the mean and covariance matrices for a multivariate gaussian are as follows
+![mu](mu.png)
+![cov](cov.png)
+
+These are unbiased estimators
+
+###(ii)
+I modeled the prior distribution of the classes by using their rate of occurrence in the training set.
+
+###(iii)
+This is the covariate matrix of class "1" displayed as a heat map.
+![Q3bii](Q3bii.png)
+The image shows that the matrix is symmetric and semidefinite.
+
+###(iv)
+
---
+++ q3b.m
@@ -0,0 +1,91 @@
+clc
+clear all
+close all
+
+%load training data
+trainData = load('/Users/Amy/Dropbox/CS189/hw3/data/train_small.mat');
+trainData = trainData.train;
+
+%load and reshape testing data
+testData = load('/Users/Amy/Dropbox/CS189/hw3/data/test.mat');
+testData = testData.test;
+imagesize = size(testData.images);
+n = imagesize(3);
+testFeatures = zeros(n, 784);
+
+testLabels = testData.labels;
+
+for i = [1:n]
+    img = testData.images(:, :, i);
+    imgVector = reshape(img, 1, []);
+    testFeatures(i, :) = imgVector;
+end
+
+for i = 1:n
+    row = testFeatures(i, :);
+    testFeatures(i, :)= row/norm(row);
+end
+    
+%loop through all small training sets
+setSizes = [100 200 500 1000 2000 5000 10000];
+setError = zeros(1,7);
+set_mu = zeros(7, 10, 784);
+for set = [1:7]
+    %reshape training data
+    trainFeatures = zeros(setSizes(set), 784);
+    labels = trainData{set}.labels;
+    
+    for i = [1:setSizes(set)]
+        img = trainData{set}.images(:, :, i);
+        imgVector = reshape(img, 1, []);
+        trainFeatures(i, : ) = imgVector;
+    end
+    
+    mu_digits = [];
+    sigma_digits = zeros(10, 784, 784);
+    t = tabulate(labels);
+    
+    % sort data
+    for i = 0:9
+        slice = trainFeatures([labels == i], :);
+        %normalize
+        slice = slice/norm(slice);
+        %fit gaussian
+        mu = mean(slice);
+        sigma = cov(slice);
+        mu_digits = [mu_digits; mu];
+        sigma_digits(i+1, :, :) = sigma;
+    end
+    set_mu(set, :, :) = mu_digits;
+    
+    %calculate priors
+
+    priors = t(:, 2) / setSizes(set);
+    
+    %sigma overall
+
+    error = 0;
+    probabilities = zeros(10000, 10);
+        for class = 0:9
+            sigma = sigma_digits(class+1);
+            sigma= 5*(sigma + eye(784)*.005);
+            g = mvnpdf(testFeatures, mu_digits(class+1, :), sigma);
+            probabilities(:, class+1) = g.*priors(class+1);
+        end
+        %predict
+        max_pr = max(probabilities, [], 2);
+    
+    for i = 1:10000
+        prediction = find([probabilities(i, :) == max_pr(i)]) -1;
+        if(prediction ~= testLabels(i))
+            error = error + 1;
+        end 
+    end
+    setError(set) = error/10000
+
+    
+end
+
+%plot
+
+scatter(setSizes, setError);
