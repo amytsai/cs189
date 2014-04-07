73f5da92f4de56964a86275c8dfaf3a555c00b52
Allen Li <allen@allen.li>
2014-04-07 01:19:50 -0700

    implement cross-validation

--- hw5.py
+++ hw5.py
@@ -4,6 +4,8 @@ import numpy as np
 import code
 from decisions import *
 
+KFOLD = 4
+
 leaf = DecisionTree.leaf # for convenience
 def train(data, labels):
 	return grow_tree(data,labels)
@@ -11,7 +13,7 @@ def train(data, labels):
 def predict(data, labels, DT):
 	error = 0
 	predictions = np.zeros(data.shape[0])
-	for ex in range(0, data.shape[0]):
+	for ex in xrange(0, data.shape[0]):
 		prediction = DT.choose(data[ex])
 		predictions[ex] = prediction
 		if(prediction != labels[ex, 0]):
@@ -40,8 +42,35 @@ def main():
 			error += 1
 	print "training error rate = ",  error/ float(xtrain.shape[0])
 
-	#print DT
+	print "Beginning %u-fold cross-validation..." % KFOLD
+	random_classes = np.random.random_integers(0, KFOLD - 1, len(xtrain))
+	cross_trees = []
+	errors = []
+	for i in xrange(KFOLD):
+		print "Cross-validation iteration %u" % i
+		train_indices = (random_classes != i).nonzero()
+		test_indices = (random_classes == i).nonzero() # apparently boolean advanced indexing is faster than this, but meh :(
+		train_x = xtrain[train_indices]
+		train_y = ytrain[train_indices]
+		test_x = xtrain[test_indices]
+		test_y = ytrain[test_indices]
+		print "Growing tree..."
+		tree = grow_tree(train_x, train_y)
+		print "Tree grown. "
+		error = 0
+		for i in xrange(len(test_x)):
+			if tree.choose(test_x[i]) != test_y[i]:
+				error += 1
+		error_rate = float(error) / float(len(test_x))
+
+		print "Error rate: " + str(error_rate)
+		errors.append(error_rate)
+		cross_trees.append(tree)
+
+	print "Total average error rate: " + str(sum(errors) / len(errors))
+
 	code.interact(local = locals())
+
 
 
 if __name__ == "__main__":
