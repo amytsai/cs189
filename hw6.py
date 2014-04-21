045e107f6d758a089df1dcdbd848ecede4fe2575
Amy <amy.tsai@berkeley.edu>
2014-04-21 16:22:13 -0700

    added graphing things

--- hw6.py
+++ hw6.py
@@ -3,15 +3,22 @@ import scipy.io as sio
 import numpy as np
 import code
 import sys, traceback, pdb
+import matplotlib.pyplot as plt
 from math import *
 
-def train_single_layer(images, labels, epochs):
+def train_single_layer(images, labels, t_images, t_labels, isMeanSquare, epochs):
   """
   train a single layer neural network
   (mean square error)
   """
-  alpha = .07
+  # for graphing
+  train_errors = []
+  test_errors = []
+  eps = []
+
+  alpha = .008
   images_t = images.transpose()
+
   #convert labels to 10 dimensional vector
   t = np.zeros([60000, 10])
   for i in range(0, 60000):
@@ -19,40 +26,63 @@ def train_single_layer(images, labels, epochs):
     t_k[labels[i]] = 1
     t[i] = t_k
   t = t.transpose()
-  #initialize weights to be +- ~10^-3
+
+  #initialize weights to be +- ~10^-5
   W = np.random.rand(784, 10) * .00002
   bias = np.random.rand(1,10) * .002
   bias = bias - .001
   W = W - .00001
 
   for e in range(0, epochs):
+    # take a mini batch
     sample = np.arange(60000)
     np.random.shuffle(sample)
     sample = sample[0:200]
     t_batch = t[:, sample]
     images_batch = images[:, sample]
-    W_t = W.transpose()
+    
     #forward pass
+    W_t = W.transpose()
     S = np.add(np.dot(W_t, images_batch),bias.transpose())
     g_S = 1 / (1 + np.exp(-S))
-    output = g_S > .5
 
     #calculate error every 10 epochs
     if e % 10 == 0:
+      #training error
       temp = np.add(np.dot(W_t, images), bias.transpose())
       temp = 1/ (1 + np.exp(-temp))
       temp = temp > .5
-      temp = np.power(temp - t,  2)
-      error = .1 * temp.sum()
+      if isMeanSquare:
+        temp = np.power(temp - t,  2)
+        error = .1 * temp.sum()
+
       error = error/ 60000
+      train_errors.append(error)
+      
+      #test error
+      t_error = np.add(np.dot(W.transpose(), t_images), bias.transpose())
+      t_g_S = 1 / (1 + np.exp(-t_error))
+      t_output = np.argmax(t_g_S, 0)
+      error_rate = np.not_equal(t_output, t_labels)
+      error_rate = error_rate.sum()
+      error_rate = error_rate / 10000.0
+      test_errors.append(error_rate)
+      eps.append(e)
+
       print 'error at epoch ', e, 'is: ', error
 
     #backwards pass
-    delta = (g_S - t_batch) * (1 - g_S)
+    if isMeanSquare:
+      delta = (g_S - t_batch) * (1 - g_S)
+    else:
+      delta = (g_S - t_batch)
     a = alpha / pow(e + 1, .3)
+    #update
     W = W - a * np.dot(images_batch, delta.transpose())
     bias = bias - a * delta.sum(1)
 
+  plt.plot(eps, train_errors, 'g', eps, test_errors, 'b')
+  plt.show()
   return W, bias
 
 def predict_single_layer(images, labels, W, bias):
@@ -71,6 +101,7 @@ def main():
   """
   load data
   """
+  #preprocessing
   training = sio.loadmat('train.mat')
   training = training['train']
   images = training['images']
@@ -99,7 +130,7 @@ def main():
   test_labels = test_labels[0][0]
   test_labels = test_labels.flatten()
 
-  W, bias = train_single_layer(images,labels,1000)
+  W, bias = train_single_layer(images,labels, test_images, test_labels, True, 800)
   g_S, output = predict_single_layer(test_images, test_labels, W, bias)
 
   code.interact(local = locals())
