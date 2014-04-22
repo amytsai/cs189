fd0648620d14dc35841c1328317471449b767c99
Amy <amy.tsai@berkeley.edu>
2014-04-21 23:50:57 -0700

    multilayer

--- hw6.py
+++ hw6.py
@@ -203,6 +203,261 @@ def predict_single_layer(images, labels, W, bias):
   print "TEST ERROR: ", error_rate
   return g_S, output
 
+def train_multilayer_ms(images, labels, t_images, t_labels, epochs):
+  #preprocessing specific to mean square
+  means = np.average(images, 1).reshape(784, 1)
+  images = np.subtract(images, means)
+  stds = np.std(images, 1)
+  for col in range(0, images.shape[0]):
+    column = images[col, :]
+    if stds[col] != 0.0:
+      column = column / stds[col]
+    images[col, :] = column
+
+  means = np.average(t_images, 1).reshape(784, 1)
+  stds = np.std(t_images, 1)
+  for col in range(0, t_images.shape[0]):
+    column = t_images[col, :]
+    if stds[col] != 0.0:
+      column = column / stds[col]
+    t_images[col, :] = column
+
+  # for graphing
+  train_errors = []
+  train_loss = []
+  test_errors = []
+  eps = []
+
+  alpha = .05
+  images_t = images.transpose()
+
+  #convert labels to 10 dimensional vector
+  t = np.zeros([60000, 10])
+  for i in range(0, 60000):
+    t_k = np.zeros(10)
+    t_k[labels[i]] = 1
+    t[i] = t_k
+  t = t.transpose()
+
+  #initialize weights and biases
+  W1 = np.random.rand(784, 300) * 2 - 1
+  W2 = np.random.rand(300, 100) * 2 - 1
+  W3 = np.random.rand(100, 10) * 2 - 1
+  bias1 = np.random.rand(1,300) * .2 - .1
+  bias2 = np.random.rand(1, 100) * .2 - .1
+  bias3 = np.random.rand(1, 10) * .2 - .1
+
+  for e in range(0, epochs):
+    # take a mini batch
+    sample = np.arange(60000)
+    np.random.shuffle(sample)
+    sample = sample[0:200]
+    t_batch = t[:, sample]
+    images_batch = images[:, sample]
+    
+    #forward pass
+    W1_t = W1.transpose()
+    W2_t = W2.transpose()
+    W3_t = W3.transpose()
+    S1 = np.add(np.dot(W1_t, images_batch),bias1.transpose()) #S1 300 x 200
+    out1 = np.tanh(S1)
+    out1_p = 1.0 - np.power(np.tanh(S1), 2)
+    S2 = np.add(np.dot(W2_t, out1), bias2.transpose()) #S2 200 x 100
+    out2 = np.tanh(S2)
+    out2_p = 1.0 - np.power(np.tanh(S2), 2)
+    S3 = np.add(np.dot(W3_t, out2), bias3.transpose())
+    out3 = 1.0 / (1.0 + np.exp(-S3))
+
+    #assert False
+
+    #backwards pass
+    a = alpha / pow(e + 1, .3)
+    delta3 = (out3 - t_batch) * out3 * (1.0 - out3)
+    W3 = W3 - a * np.dot(S2, delta3.transpose())
+    delta2 = out2_p * np.dot(W3, delta3)
+    W2 = W2 - a * np.dot(S1, delta2.transpose())
+    delta1 = out1_p * np.dot(W2, delta2)
+    W1 = W1 - a * np.dot(images_batch, delta1.transpose())
+
+    bias1 = bias1 - a * delta1.sum(1)
+    bias2 = bias2 - a * delta2.sum(1)
+    bias3 = bias3 - a * delta3.sum(1)
+
+    #finite differences
+    '''
+    epsilon = .00001
+    out1 = predict_multilayer_me(images_batch, W1 + epsilon, W2, W3, bias1, bias2, bias3)
+    out2 = predict_multilayer_me(images_batch, W1 - epsilon, W2, W3, bias1, bias2, bias3)
+    E1 = np.power(t_batch - out1, 2).sum()
+    E2 = np.power(t_batch - out2, 2).sum()
+    fd_delta1 = (E1 - E2)/ 2* epsilon
+    '''
+
+    #calculate error every 10 epochs
+    if e % 10 == 0:
+      #training error
+      output = predict_multilayer_me(images, W1, W2, W3, bias1, bias2, bias3)
+      y = np.argmax(output, 0)
+      error = np.not_equal(y, labels)
+      error = error.sum() / 60000.0
+      loss = np.power(t - output, 2)
+      loss = .1 * error.sum()
+      loss = loss/ 60000.0
+      train_errors.append(error)
+      train_loss.append(loss)
+      
+      #test error
+      t_output = predict_multilayer_me(t_images, W1, W2, W3, bias1, bias2, bias3)
+      t_output = np.argmax(t_output, 0)
+      error_rate = np.not_equal(t_output, t_labels)
+      error_rate = error_rate.sum() / 10000.0
+      test_errors.append(error_rate)
+      eps.append(e)
+
+      print 'training error at epoch ', e, 'is: ', error
+      print 'test error at epoch ', e, 'is: ', error_rate
+
+
+  plt.plot(eps, train_errors, 'g', eps, train_loss, 'r', eps, test_errors, 'b')
+  plt.show()
+  return W, bias
+
+def train_multilayer_ce(images, labels, t_images, t_labels, epochs):
+  print 'max images: ', images.max()
+  images = images / 255.0
+  print 'max images: ', images.max()
+  means = np.average(images, 1).reshape(784, 1)
+  images = np.subtract(images, means)
+  stds = np.std(images, 1)
+  for col in range(0, images.shape[0]):
+    column = images[col, :]
+    if stds[col] != 0.0:
+      column = column / stds[col]
+    images[col, :] = column
+
+  t_images = t_images/ 255.0
+  means = np.average(t_images, 1).reshape(784, 1)
+  stds = np.std(t_images, 1)
+  for col in range(0, t_images.shape[0]):
+    column = t_images[col, :]
+    if stds[col] != 0.0:
+      column = column / stds[col]
+    t_images[col, :] = column
+
+  # for graphing
+  train_errors = []
+  train_loss = []
+  test_errors = []
+  eps = []
+
+  alpha = .005
+  images_t = images.transpose()
+
+  #convert labels to 10 dimensional vector
+  t = np.zeros([60000, 10])
+  for i in range(0, 60000):
+    t_k = np.zeros(10)
+    t_k[labels[i]] = 1
+    t[i] = t_k
+  t = t.transpose()
+
+  #initialize weights and biases
+  W1 = np.random.rand(784, 300) * .00002 - .00001
+  W2 = np.random.rand(300, 100) * .00002 - .00001
+  W3 = np.random.rand(100, 10) * .00002 - .00001
+  bias1 = np.random.rand(1,300) * .2 - .1
+  bias2 = np.random.rand(1, 100) * .2 - .1
+  bias3 = np.random.rand(1, 10) * .2 - .1
+
+  for e in range(0, epochs):
+    # take a mini batch
+    sample = np.arange(60000)
+    np.random.shuffle(sample)
+    sample = sample[0:200]
+    t_batch = t[:, sample]
+    images_batch = images[:, sample]
+    
+    #forward pass
+    W1_t = W1.transpose()
+    W2_t = W2.transpose()
+    W3_t = W3.transpose()
+    S1 = np.add(np.dot(W1_t, images_batch),bias1.transpose()) #S1 300 x 200
+    out1 = np.tanh(S1)
+    out1_p = 1 - np.power(np.tanh(S1), 2)
+    S2 = np.add(np.dot(W2_t, out1), bias2.transpose()) #S2 200 x 100
+    out2 = np.tanh(S2)
+    out2_p = 1 - np.power(np.tanh(S2), 2)
+    S3 = np.add(np.dot(W3_t, out2), bias3.transpose())
+    out3 = 1.0 / (1.0 + np.exp(-S3))
+
+    #assert False
+
+    #backwards pass
+    a = alpha / pow(e + 1, .5)
+    delta3 = (out3 - t_batch)
+    W3 = W3 - a * np.dot(S2, delta3.transpose())
+    delta2 = out2_p * np.dot(W3, delta3)
+    W2 = W2 - a * np.dot(S1, delta2.transpose())
+    delta1 = out1_p * np.dot(W2, delta2)
+    W1 = W1 - a * np.dot(images_batch, delta1.transpose())
+
+    bias1 = bias1 - a * delta1.sum(1)
+    bias2 = bias2 - a * delta2.sum(1)
+    bias3 = bias3 - a * delta3.sum(1)
+
+    #finite differences
+    '''
+    epsilon = .00001
+    out1 = predict_multilayer_me(images_batch, W1 + epsilon, W2, W3, bias1, bias2, bias3)
+    out2 = predict_multilayer_me(images_batch, W1 - epsilon, W2, W3, bias1, bias2, bias3)
+    E1 = np.power(t_batch - out1, 2).sum()
+    E2 = np.power(t_batch - out2, 2).sum()
+    fd_delta1 = (E1 - E2)/ 2* epsilon
+    '''
+
+    #calculate error every 10 epochs
+    if e % 10 == 0:
+      #training error
+      output = predict_multilayer_me(images, W1, W2, W3, bias1, bias2, bias3)
+      y = np.argmax(output, 0)
+      error = np.not_equal(y, labels)
+      error = error.sum() / 60000.0
+      loss = t * np.log(output + .00000000000001) + (1.0-t) * np.log(1.0 - output + .00000000000001) # log + small value
+      loss = .1 * error.sum()
+      loss = loss/ 60000.0
+      train_errors.append(error)
+      train_loss.append(loss)
+      
+      #test error
+      t_output = predict_multilayer_me(t_images, W1, W2, W3, bias1, bias2, bias3)
+      t_output = np.argmax(t_output, 0)
+      error_rate = np.not_equal(t_output, t_labels)
+      error_rate = error_rate.sum() / 10000.0
+      test_errors.append(error_rate)
+      eps.append(e)
+
+      print 'training error at epoch ', e, 'is: ', error
+      print 'test error at epoch ', e, 'is: ', error_rate
+
+
+  plt.plot(eps, train_errors, 'g', eps, train_loss, 'r', eps, test_errors, 'b')
+  plt.show()
+  return W, bias
+
+def predict_multilayer_me(images, W1, W2, W3, b1, b2, b3):
+  W1_t = W1.transpose()
+  W2_t = W2.transpose()
+  W3_t = W3.transpose()
+  S1 = np.add(np.dot(W1_t, images),b1.transpose()) #S1 300 x 200
+  out1 = np.tanh(S1)
+  out1_p = 1 - np.power(np.tanh(S1), 2)
+  S2 = np.add(np.dot(W2_t, out1), b2.transpose()) #S2 200 x 100
+  out2 = np.tanh(S2)
+  out2_p = 1 - np.power(np.tanh(S2), 2)
+  S3 = np.add(np.dot(W3_t, out2), b3.transpose())
+  out3 = 1.0 / (1.0 + np.exp(-S3))
+  return out3
+
 def main():
   """
   load data
@@ -215,8 +470,6 @@ def main():
   images = images[0][0]
   images = images.reshape(28*28, 60000)
   images = images.astype(float)
-
-
   labels = labels[0][0]
   labels = labels.flatten()
 
@@ -227,16 +480,13 @@ def main():
   test_images = test_images[0][0]
   test_images = test_images.reshape(28*28, 10000)
   test_images = test_images.astype(float)
-
-
   test_labels = test_labels[0][0]
   test_labels = test_labels.flatten()
 
-  W, bias = train_single_layer_ce(images,labels, test_images, test_labels, 500)
+  W, bias = train_multilayer_ce(images,labels, test_images, test_labels, 100)
   g_S, output = predict_single_layer(test_images, test_labels, W, bias)
 
   code.interact(local = locals())
-
 
 
 if __name__ == "__main__":
